a3935e93e77105d8f590da9f22471b3b3780c6e2
Scotty Logan <swl@stanford.edu>
2015-06-22 11:09:21 -0700

    bumped stable version, updated to handle new naming scheme

--- manifests/stable.pp
+++ manifests/stable.pp
@@ -7,17 +7,17 @@
 # or with specific version
 #
 #   class { 'iTerm':
-#     version => '2_0'
+#     version => '2_1_1'
 #   }
 #
 class iterm2::stable (
   $ensure  = 'present',
-  $version ='2_0'
+  $version ='2_1_1'
 ) {
   package { 'iTerm':
     ensure   => $ensure,
     flavor   => 'zip',
     provider => 'compressed_app',
-    source   => "http://www.iterm2.com/downloads/stable/iTerm2_v${version}.zip"
+    source   => "https://www.iterm2.com/downloads/stable/iTerm2-${version}.zip"
   }
 }
--- spec/classes/iterm2_stable_spec.rb
+++ spec/classes/iterm2_stable_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe 'iterm2::stable' do
   it do
-    version = '2_0'
+    version = '2_1_1'
     should contain_package('iTerm').with({
       :ensure   => 'present',
       :flavor   => 'zip',
       :provider => 'compressed_app',
-      :source   => "http://www.iterm2.com/downloads/stable/iTerm2_v#{version}.zip"
+      :source   => "https://www.iterm2.com/downloads/stable/iTerm2-#{version}.zip"
     })
   end
 end
